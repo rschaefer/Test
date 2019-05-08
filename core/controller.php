02edf912e52de615e8490a61fc82472417ddff1f
rschaefer <rita.d.schaefer@gmail.com>
2019-05-08 11:07:17 +0200

    Version 01

---
+++ core/controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Summary (no period for file headers)
+ *
+ * Description. (use period)
+ *
+ * @link URL
+ *
+ * @package FCK
+ * @subpackage ${PACKAGE_NAME}
+ *
+ * @author: rita
+ * @version: x.x.x
+ */
